37db98fed1651b63a4799d31eba4e6e63516296c
Jesus Eduardp <eduardoCastillo180@hotmail.com>
2022-10-23 21:03:08 -0400

    warning rel fixed

--- src/components/buttonToTalkWap.js
+++ src/components/buttonToTalkWap.js
@@ -4,7 +4,7 @@ import "./buttonToTalkWap.scss"
 function ButtonToTalk (props){
     return(
         <div className={props.buttonClass}>
-          <a aria-label="Chat on WhatsApp" href={props.link} target="_blank" rel="chat">
+          <a aria-label="Chat on WhatsApp" href={props.link} rel="noreferrer" target="_blank" >
           <span className={props.icon}></span>
             {props.children}</a> 
            
